050147249dc2e504ae0a9d8db3c247e32c400d83
Gautam <gau7049@gmail.com>
2024-04-05 08:33:40 +0530

    url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:8000", {
+            const socket = io("https://chat-app-prod-l3cr.onrender.com/", {
                 query:{
                     userId: authUser._id
                 }
